8d49066a1f14e3bea8ece789e64285dfbcca7bd7
ameralshabebi <69112877+ameralshabebi@users.noreply.github.com>
2022-07-05 01:24:12 +0300

    add mada,stcpay,applepay icons

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const loadIcons = () => {
 		"<div style='width:auto;'> <img style='max-width:210px; max-height:100px; margin-top:15px; margin-right:5px;' src='https://i.ibb.co/Wxd9d2W/visa54x34.png'></img> </div>",
 		"<div style='width:auto;'> <img style='max-width:210px; max-height:100px; margin-top:15px; margin-right:5px;' src='https://i.ibb.co/Wxd9d2W/visa54x34.png'></img> </div></div>",
 	];
-	const icons = iconsArray.join(',');
+	let userAgent = navigator.userAgent;
 	console.log(icons);
+	if (userAgent.match(/safari/i)) {
+		let apple_pay_icon =
+			"<div style='width:auto;'> <img style='max-width:210px; max-height:100px; margin-top:15px; margin-right:5px;' src='https://i.ibb.co/Wxd9d2W/visa54x34.png'></img> </div>";
+		iconsArray.push(apple_pay_icon);
+	}
+	let icons = iconsArray.join('');
 	// what should be passed is your app id in the seclector below
-	document.querySelector(
-		`label.ec-radiogroup__item--app_id-custom-app-76468716-1 div.ec-radiogroup__info:empty`
-	).innerHTML = icons;
+	// document.querySelector(
+	// 	`label.ec-radiogroup__item--app_id-custom-app-76468716-1 div.ec-radiogroup__info:empty`
+	// ).innerHTML = icons;
 };
 
 // Ecwid.OnPageLoaded.add(function (page) {
