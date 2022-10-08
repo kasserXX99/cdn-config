9fea88e73223d42991c608600b0bd3ef204edaf5
ameralshabebi <69112877+ameralshabebi@users.noreply.github.com>
2022-10-08 23:55:32 +0300

    adding tamara svg

---
+++ tamara.js
@@ -0,0 +1,35 @@
+Ecwid.OnPageLoaded.add(function (page) {
+	if (page.type === 'PRODUCT' || page.name === 'PRODUCT') {
+		infoWidget();
+	}
+	if (page.type == 'CHECKOUT_PAYMENT_DETAILS') {
+	}
+});
+
+const checkoutWidget = () => {
+	let checkoutBox = document.querySelector(
+		'label.ec-radiogroup__item--app_id-custom-app-64158187-1 div.ec-radiogroup__info:empty'
+	);
+};
+
+const infoWidget = () => {
+	let insert = document.querySelector('.details-product-purchase');
+	let div = document.createElement('div');
+	div.style.marginTop = '20px';
+	div.style.width = '83%';
+	let inn = `<div class="tamara-widget" data-lang="ar" data-country-code="SA" data-currency="SAR" data-color-type="default" data-payment-type="installment" data-number-of-installments="3" data-installment-available-amount="99"></div>`;
+	div.innerHTML = inn;
+	insert.appendChild(div);
+	let script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'https://cdn.tamara.co/widget/tamara-widget.min.js';
+	document.head.appendChild(script);
+	setTimeout(() => {
+		if (window.TamaraProductWidget) {
+			window.TamaraProductWidget.render();
+			document.querySelector('.tamara-widget').style.textAlign = 'center';
+		}
+	}, 2000); // Waiting for 2s - Make sure Tamara's widget is installed
+};
+// add the widget on the discription section .product-details__general-info
+// add the widget next to the add to cart button .details-product-purchase
